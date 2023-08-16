3f0bd191d620c8fdf56225c7b82c3c66cd7c8bb9
inottn <inottn@outlook.com>
2023-08-16 22:12:23 +0800

    test: add test case

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,10 +1,33 @@
 import { describe, expect, it } from 'vitest';
-import './mock';
+import { mockRequest } from './mock';
 import { Request } from '../src';
 
 describe('miniapp-request', () => {
   it('get', () => {
     const http = new Request();
-    expect(http.get('test')).resolves.toBe(1);
+    const response = {
+      headers: {},
+      status: 200,
+      data: 'test',
+    };
+    mockRequest(response);
+    expect(http.get('test')).resolves.toMatchObject(response);
+  });
+
+  it('request interceptor', async () => {
+    const http = new Request();
+    const response = {
+      headers: {},
+      status: 200,
+      data: 1,
+    };
+    http.interceptors.request.use((config) => {
+      config.headers.token = 'token';
+      return config;
+    });
+    const spy = mockRequest(response);
+    await http.get('test');
+    const { lastCall } = spy.mock;
+    expect(lastCall?.[0]).toMatchObject({ headers: { token: 'token' } });
   });
 });
--- test/mock.ts
+++ test/mock.ts
@@ -1,10 +1,21 @@
 import { vi } from 'vitest';
-import type { TransformedConfig } from 'src/types';
+import type { RawResponse, TransformedConfig } from 'src/types';
+
+declare const my: any;
+
+let stubResponse: RawResponse;
 
 vi.stubGlobal('my', {
   request: (config: TransformedConfig) => {
-    config.success(1);
+    config.success(stubResponse);
   },
   uploadFile: vi.fn(),
   downloadFile: vi.fn(),
 });
+
+const spy = vi.spyOn(my, 'request');
+
+export function mockRequest(response: RawResponse) {
+  stubResponse = response;
+  return spy;
+}
