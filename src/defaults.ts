1ef66456845bc6fcf85e0b9602aeceb7d2e8dcf7
inottn <inottn@outlook.com>
2023-08-27 14:16:16 +0800

    feat: add default transformRequest

--- src/defaults.ts
+++ src/defaults.ts
@@ -1,9 +1,26 @@
+import { isObject, isUndefined } from '@inottn/fp-utils';
 import adapter from './adapter';
+import type { InstanceConfig } from './types';
 
-export default {
+const defaultConfig: InstanceConfig = {
   adapter,
   headers: { 'content-type': 'application/json' },
+  transformRequest: [
+    function defaultTransformRequest(data) {
+      if (isObject(data)) {
+        for (const key of Object.keys(data)) {
+          if (isUndefined(data[key])) {
+            delete data[key];
+          }
+        }
+      }
+
+      return data;
+    },
+  ],
   validateStatus: (status: number) => {
     return status >= 200 && status < 300;
   },
 };
+
+export default defaultConfig;
